bdf3b8b322ead9a714669a5754f554fde59e2679
Russell Green <russell.green@scasurgery.com>
2021-02-07 16:36:03 -0600

    Working example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,60 +1,69 @@
-import { Component, ViewChild, ElementRef, OnInit, AfterViewInit } from '@angular/core';
+import {
+  Component,
+  ViewChild,
+  ElementRef,
+  OnInit,
+  AfterViewInit,
+} from '@angular/core';
 import { IFile } from './dto/IFile';
-import { SelectableSettings, RowArgs, SelectionEvent } from '@progress/kendo-angular-grid';
+import {
+  SelectableSettings,
+  RowArgs,
+  SelectionEvent,
+} from '@progress/kendo-angular-grid';
 import { Observable } from 'rxjs';
-
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  @ViewChild('viewer', {static: false}) viewer: ElementRef;
-  path:string = '';
-  extension: string = ''
-  selectedFile$: Observable<IFile> = {}as Observable<IFile>;
+  @ViewChild('viewer', { static: false }) viewer: ElementRef;
+  path: string = '';
+  extension: string = '';
+  selectedFile$: Observable<IFile> = {} as Observable<IFile>;
   selectedFile: IFile;
   title = 'my-first-angular-project';
-  files:IFile[] = [{
-    fileName: 'Book1',
-    extension: 'XLSX',
-    path: '/../assets/Book1.xlsx'
-  },
-  {
-    fileName: 'Test Doc',
-    extension: 'DOCX',
-    path: '/../assets/Test Doc.docx'
-  },
-  {
-    fileName: 'webviewer',
-    extension: 'PDF',
-    path: '/../assets/Resume.pdf'
-  }];
+  files: IFile[] = [
+    {
+      fileName: 'Book1',
+      extension: 'XLSX',
+      path: '/../assets/Book1.xlsx',
+    },
+    {
+      fileName: 'Test Doc',
+      extension: 'DOCX',
+      path: '/../assets/Test Doc.docx',
+    },
+    {
+      fileName: 'COLONBXMEDOFFMD',
+      extension: 'PDF',
+      path: '/../assets/COLONBXMEDOFFMD.pdf',
+    },
+  ];
 
-  public isRowSelected = (e: RowArgs) => this.selectedFile ? this.selectedFile.fileName  === e.dataItem.fileName : false;
-
+  public isRowSelected = (e: RowArgs) =>
+    this.selectedFile
+      ? this.selectedFile.fileName === e.dataItem.fileName
+      : false;
 
   public selectableSettings: SelectableSettings;
 
   constructor() {
-      this.setSelectableSettings();
+    this.setSelectableSettings();
   }
 
   public setSelectableSettings(): void {
-   
-      this.selectableSettings = {
-          checkboxOnly: false,
-          mode: 'single'
-      };
+    this.selectableSettings = {
+      checkboxOnly: false,
+      mode: 'single',
+    };
   }
 
-  onSelectionChange(e: SelectionEvent){
+  onSelectionChange(e: SelectionEvent) {
     this.selectedFile = e.selectedRows[0].dataItem;
-    
   }
 
-  ngOnInit(){
-
-  }
+  ngOnInit() {}
 }
